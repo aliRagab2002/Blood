4059a35dae9b7171019bb6f012ff1e4d7053f45c
aliRagab2002 <tltaath30@gmail.com>
2024-02-23 07:09:59 +0200

    up

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require("cors")
+app.use(cors())
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 require('./db/db')
 app.use(express.json())
